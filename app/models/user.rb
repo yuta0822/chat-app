d1c0029b711ac742a620e0647cb7f869e28228ab
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-01-15 15:59:31 +0900

    結合テストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   has_many :room_users
-  has_many :users, through: :room_users
+  has_many :rooms, through: :room_users
   has_many :messages
   
 end
