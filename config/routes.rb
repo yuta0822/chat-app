f3f970780054e0ad7d2bdd77d1e543d42f88f2da
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-01-15 12:15:23 +0900

    各モデルのテスト

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
+  end
 end
